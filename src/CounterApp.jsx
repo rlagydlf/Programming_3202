41e10bbe3165ef085448c103e05b0d16e1e136b9
djaakrk <01021139593a@gmail.com>
2026-03-31 09:58:16 +0900

    CounterApp+(최대 10까지), if문, 삼항연산자, Math.min()

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -15,6 +15,14 @@ function CounterApp() {
     <button onClick={() => setCount((count) => count + 2)}>+2</button>
     <button onClick={() => setCount((count) => count - 1)}>-</button>
     <button onClick={() => setCount(0)}>reset</button>
+    <button onClick={() => setCount((count) => {
+      if(count < 10){
+        return count + 1
+      }
+      return count
+    })}>+(최대 10까지)</button>
+    <button onClick={() => setCount((count) => count<10 ? count + 1 : count)}>+(최대 10까지)</button>
+    <button onClick={() => setCount((count) => Math.min(count+1, 10))}>+(최대 10까지)</button>
       {/* {<button
         className="counter"
         onClick={() => setCount((count) => count + 1)} // JS 함수호출X; 함수정의O
