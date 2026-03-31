76b72a5261f69c53f99e338a2e88b983cf312f59
djaakrk <01021139593a@gmail.com>
2026-03-31 10:11:15 +0900

    CounterApp plusMax10

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -7,22 +7,32 @@ import heroImg from './assets/hero.png'
 function CounterApp() {
   // count: 이전 또는 현재 값. setCount(): count 값 변경 함수. 반드시 이걸 거쳐야 함
   const [count, setCount] = useState(0)
-
-  return (
-    <>
-    <h1>{count}</h1>
-    <button onClick={() => setCount((count) => count + 1)}>+</button>
-    <button onClick={() => setCount((count) => count + 2)}>+2</button>
-    <button onClick={() => setCount((count) => count - 1)}>-</button>
-    <button onClick={() => setCount(0)}>reset</button>
-    <button onClick={() => setCount((count) => {
-      if(count < 10){
+  function plusMax10() {
+    setCount((count) => {
+      if (count < 10) {
         return count + 1
       }
       return count
-    })}>+(최대 10까지)</button>
-    <button onClick={() => setCount((count) => count<10 ? count + 1 : count)}>+(최대 10까지)</button>
-    <button onClick={() => setCount((count) => Math.min(count+1, 10))}>+(최대 10까지)</button>
+    })
+  }
+
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((count) => count + 1)}>+</button>
+      <button onClick={() => setCount((count) => count + 2)}>+2</button>
+      <button onClick={() => setCount((count) => count - 1)}>-</button>
+      <button onClick={() => setCount(0)}>reset</button>
+      <button onClick={() => setCount((count) => {
+        if (count < 10) {
+          return count + 1
+        }
+        return count
+      })}>+(최대 10까지)</button>
+      <button onClick={() => setCount((count) => count < 10 ? count + 1 : count)}>+(최대 10까지)</button>
+      <button onClick={() => setCount((count) => Math.min(count + 1, 10))}>+(최대 10까지)</button>
+      <button onClick={() => plusMax10()}>+(최대 10까지)</button>
       {/* {<button
         className="counter"
         onClick={() => setCount((count) => count + 1)} // JS 함수호출X; 함수정의O
